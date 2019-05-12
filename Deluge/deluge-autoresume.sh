7ef26c77b423626711eb841e1897c41f07745e9d
nostyle <nostyle@bigmilky.club>
2019-05-13 02:45:25 +0930

    Update deluge-autoresume.sh

--- Deluge/deluge-autoresume.sh
+++ Deluge/deluge-autoresume.sh
@@ -3,19 +3,25 @@ torrentid=$1
 torrentname=$2
 torrentpath=$3
 
+#change all information here to reflect slot specifics
+ip=xyz.xyz.xyz.xyz
+port=zzzzz
+username=xxxx
+password=yyyy
+
 x=1
 while [ $x -le 100 ]
 do
   sleep 2
   echo "Running $x times" >> ~/script.log
   echo "TorrentID: $torrentid" >> ~/script.log
-  line=$(deluge-console "connect {ip address}:{port} {user} {pass}; info" $1 | grep "Tracker status")
+  line=$(deluge-console "connect '$ip':'$port' '$username' '$password'; info" $1 | grep "Tracker status")
   echo $line >> ~/script.log
   case "$line" in
     *Error*|*Sent*|*End*of*file*|*Bad*Gateway*)
-      deluge-console "connect {ip address}:{port} {user} {pass}; pause '$torrentid'"
+      deluge-console "connect '$ip':'$port' '$username' '$password'; pause '$torrentid'"
       sleep 5
-      deluge-console "connect {ip address}:{port} {user} {pass}; resume '$torrentid'"
+      deluge-console "connect '$ip':'$port' '$username' '$password'; resume '$torrentid'"
       ;;
     *)
       echo "Found working torrent: $torrentname $torrentpath $torrentid" >> ~/script.log
