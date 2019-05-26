68d081fbd8f473c4d650b835efa5b41efe0a3d09
no5tyle <32665563+no5tyle@users.noreply.github.com>
2019-05-27 04:30:59 +0930

    Update sonarr.sh

--- sonarr.sh
+++ sonarr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 app-sonarr stop
+cp ~/.apps/sonarr/config.xml ~/.apps/sonarr/config.xml.bak
 sed -i "s+<UrlBase></UrlBase>+<UrlBase>/sonarr</UrlBase>+g" ~/.apps/sonarr/config.xml
 app-sonarr start
