f8dc3d5f889bf107838ad83535557a0874db7858
Alpha <22331737+Alpha5K@users.noreply.github.com>
2019-06-07 13:07:14 -0400

    Update mellow-install.sh

--- Mellow/mellow-install.sh
+++ Mellow/mellow-install.sh
@@ -9,37 +9,43 @@ then
         exit
 fi 
 
-mkdir ~/.apps/nodejs
-cd ~/.apps/nodejs
+PORT=$(( 11000 + (($UID - 1000) * 50) + 30))
 
-echo "Downloading NodeJS..."
-version=$(curl -s "https://resolve-node.now.sh/lts")
-release="node-$version-linux-x64.tar.xz"
-wget -q "https://nodejs.org/dist/$version/$release"
-tar --strip-components=1 -xf $release
-rm $release
+if [ ! -d "$HOME/.nvm" ]
+then
+    echo "Installing Node..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    nvm install 12 --latest-npm
+    nvm alias default 12
+    nvm use default
+else
+    echo "Node already installed. Skipping..."
+fi
 
-echo 'export PATH=$PATH:~/.apps/nodejs/bin' >> ~/.bashrc
-export PATH=$PATH:~/.apps/nodejs/bin
-
-echo "Downloading Mellow..."
+echo "Installing Mellow..."
 git clone -b develop "https://github.com/v0idp/Mellow.git" ~/.apps/mellow
 cd ~/.apps/mellow
-
 npm install --loglevel=silent
 
-echo "Installing service..."
-mkdir -p ~/.config/systemd/user
+echo "Configuring Mellow..."
+sed -i "s/5060/$port/g" ~/.apps/mellow/src/WebServer.js
+
+echo "Installing Service..."
 echo "[Unit]
 Description=Mellow
 After=network.target
 StartLimitIntervalSec=0
+
 [Service]
 Type=simple
 Restart=on-failure
 RestartSec=10
 WorkingDirectory=$HOME/.apps/mellow
-ExecStart=$HOME/.apps/nodejs/bin/node src/index.js
+ExecStart=$(which node) src/index.js
+
 [Install]
 WantedBy=default.target" >> $HOME/.config/systemd/user/mellow.service
 systemctl --user daemon-reload
@@ -47,22 +53,18 @@ systemctl --user enable mellow
 
 loginctl enable-linger $USER
 
-echo "Updating ports..."
-port=$(( 11000 + (($UID - 1000) * 50) + 30))
-sed -i "s/5060/$port/g" ~/.apps/mellow/src/WebServer.js
-
 echo "Starting Mellow..."
 systemctl --user start mellow
 
-echo "Downloading uninstall script..."
+echo "Downloading Uninstaller..."
 cd ~
 wget -q https://raw.githubusercontent.com/no5tyle/UltraSeedbox-Scripts/master/Mellow/mellow-uninstall.sh
 chmod +x mellow-uninstall.sh
 
-echo "Cleaning up..."
+echo "Cleaning Up..."
 rm -- "$0"
 
 ip=$(curl -s "https://ipinfo.io/ip")
-printf "\033[0;31mDone!\033[0m\n"
+printf "\033[0;32mDone!\033[0m\n"
 echo "Access your Mellow installation at http://$ip:$port"
 echo "Run ./mellow-uninstall.sh to uninstall"
