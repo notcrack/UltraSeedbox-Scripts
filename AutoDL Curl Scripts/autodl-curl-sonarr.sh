a53e9ee2db93507d3c12821ec39933bacc87f658
Alpha <22331737+Alpha5K@users.noreply.github.com>
2019-06-08 15:51:50 -0400

    Update autodl-curl-sonarr.sh

--- AutoDL Curl Scripts/autodl-curl-sonarr.sh
+++ AutoDL Curl Scripts/autodl-curl-sonarr.sh
@@ -6,5 +6,5 @@ apiKey=$3
 date=$(date -u +"%Y-%m-%d %H:%M:%SZ")
 
 {
-/usr/bin/curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: $apiKey" -X POST -d '{"title":"'"$title"'","downloadUrl":"'"$downloadUrl"'","protocol":"torrent","publishDate":"'"$date"'"}' https://username.lwxxx.usbx.me/sonarr/api/release/push
+$(which curl) -i -H "Accept: application/json" -H "Content-Type: application/json" -H "X-Api-Key: $apiKey" -X POST -d '{"title":"'"$title"'","downloadUrl":"'"$downloadUrl"'","protocol":"torrent","publishDate":"'"$date"'"}' https://username.lwxxx.usbx.me/sonarr/api/release/push
 } &> /dev/null
