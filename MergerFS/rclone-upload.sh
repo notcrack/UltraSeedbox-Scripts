590973e6df725805a7de749f6d05cc88b633c633
Xan <28868448+XantherBanter@users.noreply.github.com>
2019-06-29 15:40:15 +0000

    Update rclone-upload.sh

--- MergerFS/rclone-upload.sh
+++ MergerFS/rclone-upload.sh
@@ -10,7 +10,7 @@ then
     exit
 else
     touch "$lock_file"
-    "$home_dir"/bin/rclone move /homexx/yyyyy/Stuff/Local/ gdrive: --drive-chunk-size 128M --tpslimit 10 --transfers 40 --checkers 40 --max-backlog 200000 --drive-acknowledge-abuse=true -vvv --delete-empty-src-dirs --fast-list --log-file "$home_dir"/scripts/rclone-sync.log
+    "$home_dir"/bin/rclone move /homexx/yyyyy/Stuff/Local/ gdrive: --drive-chunk-size 128M --tpslimit 10 --transfers 10 --checkers 10 --drive-acknowledge-abuse=true -vvv --delete-empty-src-dirs --fast-list --log-file "$home_dir"/scripts/rclone-sync.log
     rm -f "$lock_file"
     trap - SIGINT SIGTERM
     exit
