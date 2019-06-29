6d3a35d84198aef8f118665cde501442cd539d08
Alpha <22331737+Alpha5K@users.noreply.github.com>
2019-06-29 12:46:50 -0400

    Still is

--- MergerFS/rclone-upload.sh
+++ MergerFS/rclone-upload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-home_dir="/homexx/yyyyy"
-lock_file="$home_dir/rclone.lock"
+lock_file="$HOME/rclone.lock"
 
 trap "rm -f $lock_file; exit 0" SIGINT SIGTERM
 if [ -e "$lock_file" ]
@@ -10,7 +9,7 @@ then
     exit
 else
     touch "$lock_file"
-    "$home_dir"/bin/rclone move /homexx/yyyyy/Stuff/Local/ gdrive: --drive-chunk-size 128M --tpslimit 10 --transfers 10 --checkers 10 --drive-acknowledge-abuse=true -vvv --delete-empty-src-dirs --fast-list --log-file "$home_dir"/scripts/rclone-sync.log
+    "$HOME"/bin/rclone move /homexx/yyyyy/Stuff/Local/ gdrive: --drive-chunk-size 128M --tpslimit 10 --transfers 10 --checkers 10 --drive-acknowledge-abuse=true -vvv --delete-empty-src-dirs --fast-list --log-file "$HOME"/scripts/rclone-sync.log
     rm -f "$lock_file"
     trap - SIGINT SIGTERM
     exit
