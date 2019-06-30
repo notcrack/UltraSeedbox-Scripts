2eacda2cd4d0e9a155644039cbd364ac19429444
Xan <28868448+XantherBanter@users.noreply.github.com>
2019-06-30 17:14:55 +0000

    Added ~/tmp

--- MergerFS/mergerfs-install.sh
+++ MergerFS/mergerfs-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/tmp
 wget https://github.com/trapexit/mergerfs/releases/download/2.28.1/mergerfs_2.28.1.debian-stretch_amd64.deb -P ~/tmp
-dpkg -x mergerfs_2.28.1.debian-stretch_amd64.deb ~/tmp
+dpkg -x ~/tmp/mergerfs_2.28.1.debian-stretch_amd64.deb ~/tmp
 mv ~/tmp/usr/bin/* ~/.local/bin
 rm -rf ~/tmp
