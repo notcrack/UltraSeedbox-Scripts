e9b36f45a0c25266e29edd517bda2460a700ccc1
Xan <28868448+XantherBanter@users.noreply.github.com>
2019-07-03 06:33:13 +0000

    added delete rclone-tmp folder

--- MergerFS-Rclone/mergerfs-install.sh
+++ MergerFS-Rclone/mergerfs-install.sh
@@ -4,4 +4,5 @@ wget https://github.com/trapexit/mergerfs/releases/download/2.28.1/mergerfs_2.28
 dpkg -x ~/tmp/mergerfs_2.28.1.debian-stretch_amd64.deb ~/tmp
 mv ~/tmp/usr/bin/* ~/.local/bin
 rm -rf ~/tmp
+rm -rf ~/.rclone-tmp
 rclone version
