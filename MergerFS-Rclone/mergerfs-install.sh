3ab9cb8dae6cbe5aab05078215b899cbc6c704c4
Xan <28868448+XantherBanter@users.noreply.github.com>
2019-07-03 06:31:03 +0000

    added rclone version check

--- MergerFS-Rclone/mergerfs-install.sh
+++ MergerFS-Rclone/mergerfs-install.sh
@@ -4,3 +4,4 @@ wget https://github.com/trapexit/mergerfs/releases/download/2.28.1/mergerfs_2.28
 dpkg -x ~/tmp/mergerfs_2.28.1.debian-stretch_amd64.deb ~/tmp
 mv ~/tmp/usr/bin/* ~/.local/bin
 rm -rf ~/tmp
+rclone version
